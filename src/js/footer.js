e65ae47cae594cf2d82222988b304ed711a881e5
wanghuashui <wx15959172169@163.com>
2020-06-26 19:39:58 +0800

    底部

---
+++ src/js/footer.js
@@ -0,0 +1,150 @@
+(()=>{
+    let footer1=`<div class="site-footer">
+    <div class="container">
+        <div class="footer">
+            <ul class="list-service clearfix">
+                <li>
+                    <div class="foo-con">
+                        <img src="https://www.konka.com/public/images/29/72/32/508be9abdf01173241a65b671c4ec6fff242e359.png?72574_OW29_OH29">
+                    </div>
+                    <div class="foo-title">
+                        <h4>全场包邮</h4>
+                        <small>全场商品免费送到家</small>
+                    </div>
+                </li>
+                <li>
+                    <div class="foo-con">
+                        <img src="https://www.konka.com/public/images/8e/e7/58/dda3bbcea1734e9cc57f733bd39c7bdfa8a098c9.png?72590_OW23_OH24">
+                    </div>
+                    <div class="foo-title">
+                        <h4>质量保证</h4>
+                        <small>享受国家三包政策</small>
+                    </div>
+                </li>
+                <li>
+                    <div class="foo-con">
+                        <img src="https://www.konka.com/public/images/0a/f6/6b/fa606e57f51c1c20bee1b08b460b546a2308535a.png?72616_OW24_OH24">
+                    </div>
+                    <div class="foo-title">
+                        <h4>权益保障</h4>
+                        <small>7天包退15天包换</small>
+                    </div>
+                </li>
+                <li>
+                    <div class="foo-con">
+                        <img src="https://www.konka.com/public/images/18/f6/51/e7f1acfff43fd63737b75f9dd040c04875dbef80.png?72627_OW24_OH24">
+                    </div>
+                    <div class="foo-title">
+                        <h4>放心服务</h4>
+                        <small>24小时内上门服务</small>
+                    </div>
+                </li>
+                <li>
+                    <div class="foo-con">
+                        <img src="https://www.konka.com/public/images/e1/df/92/2304a231ed8691bc454c367e282052810d5634d0.png?72639_OW25_OH25">
+                    </div>
+                    <div class="foo-title">
+                        <h4>意外惊喜</h4>
+                        <small>限时打折优惠不断</small>
+                    </div>
+                </li>
+            </ul>
+        </div>
+        <div class="footer-links clearfix">
+            <dl>
+                <dt>帮助中心</dt>
+                <dd><a href="n-31-alist.html" target="_blank">购买指南</a></dd>
+                <dd><a href="n-32-alist.html" target="_blank">支付方式</a></dd>
+                <dd><a href="n-33-alist.html" target="_blank">配送方式</a></dd>
+                <dd><a href="n-34-alist.html" target="_blank">售后政策</a></dd>
+                <dd><a href="http://www.konkasmart.com/Service/show_article_noh5fwcx/cate/web_fwcx.html" target="_blank">防伪查询</a></dd>
+                <!--<dd><a href="http://shop.konka.com/member-orders.html" target="_blank">旧版官网</a></dd> -->
+            </dl>
+            <dl>
+                <dt>新闻中心</dt>
+                <dd><a href="news.html" target="_blank">集团新闻</a></dd>
+                <dd><a href="n-5-alist.html" target="_blank">媒体报道</a></dd>
+            </dl>
+            <dl>
+                <dt>投资者关系</dt>
+                <dd><a href="/n-26-alist.html" target="_blank">公司概要</a></dd>
+                <dd><a href="/n-27-alist.html" target="_blank">高管团队</a></dd>
+                <dd><a href="http://webapi.cninfo.com.cn/notice.html?id=657&amp;api=p_sysapi1091&amp;stype=1&amp;scode=000016&amp;column=SECNAME,F002V,F001D,SORTNAME&amp;sortcode=0103,0105,0107,0109,0110,0111,0113,0115,0117,0119,0120,0121,0123,0125,0127" target="_blank">最新公告</a></dd>
+                <dd><a href="/n-36-alist.html" target="_blank">公司治理</a></dd>
+            </dl>
+            <dl>
+                <dt>集团成员网站</dt>
+                <dd><a href="http://www.chinaoct.com/" target="_blank">华侨城集团</a></dd>
+                <dd><a href="http://www.4008800016.com/index" target="_blank">康佳全球服务中心</a></dd>
+                <dd><a href="http://www.konka.com.hk/" target="_blank">康佳国际营销</a></dd>
+                <dd><a href="http://www.konkamobile.com/" target="_blank">康佳移动互联科技</a></dd>
+                <dd><a href="http://www.konkaled.cn/" target="_blank">康佳壹视界商业显示</a></dd>
+                <dd><a href="http://www.konkasmart.com/" target="_blank">康佳智能电器</a></dd>
+            </dl>
+            <dl>
+                <dt>关于我们</dt>
+                <dd><a href="/about.html" target="_blank">了解康佳</a></dd>
+                <dd><a href="http://konka.hotjob.cn/wt/konka/web/index/webPositionN300!getPositionListByCorpAndWebColumnShowPic?postIdEnc=&amp;brandCode=1&amp;recruitType=2&amp;lanType=1&amp;columnId=2" target="_blank">加入康佳</a></dd>
+                <dd><a href="http://campus.51job.com/konka/flash.html" target="_blank">校园招聘</a></dd>
+                <dd><a href="contact.html" target="_blank">联系我们</a></dd>
+            </dl>
+            <dl>
+                <dt>关注我们</dt>
+                <dd><a href="https://weibo.com/konka?topnav=1&amp;wvr=6&amp;topsug=1&amp;is_all=1" target="_blank">官方微博</a></dd>
+                <dd class="showCode"><a href="#">官方微信</a>
+                    <div class="code">
+                        <img src="https://www.konka.com/themes/pc/konka/images/ewm.jpg?vf06b" alt="">
+                    </div>
+                </dd>
+                <dd><a href="http://bbs.konka.com" target="_blank">官方社区</a></dd>
+                <dd class="showCode"><a href="#">K迷俱乐部</a>
+                    <div class="code">
+                        <img src="https://www.konka.com/themes/pc/konka/images/km.jpg?vf06b" alt="">
+                    </div>
+                </dd>
+                <dd class="showCode"><a href="#">康佳微商城</a>
+                    <div class="code">
+                        <img src="https://www.konka.com/themes/pc/konka/images/wsc.jpg?vf06b" alt="">
+                    </div>
+                </dd>
+                <dd class="showCode"><a href="#">康佳服务</a>
+                    <div class="code">
+                        <img src="https://www.konka.com/themes/pc/konka/images/kafu.jpg?vf06b" alt="">
+                    </div>
+                </dd>
+                <dd><a href="http://tv.qq.com/" target="_blank">腾讯视频TV端</a></dd>
+            </dl>
+            <div class="links-iphone">
+                <p class="hotline">24小时全国服务热线</p>
+                <p class="tele">400-880-0016</p>
+                <a href="javascript:void(0)" target="_blank" class="Customer zixun">
+                    <span class="glyphicon glyphicon-headphones">购买咨询</span>
+                </a> 
+                <a style="margin-left: 10px;" target="_blank" href="http://www.4008800016.com:84/MTalk/talk/visitor/dialog?config_id=1&amp;come=KONKAGUANWANG" class="Customer">
+                    <span class="glyphicon glyphicon-user">售后服务</span>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="for-footer" style="padding:0 100px">
+    <div class="for-logo">
+        <img src="https://www.konka.com/themes/pc/konka/images/konka.png?vf06b" alt="">
+    </div>
+    <div class="for-link">
+        <p>Copyright © 1980-2020 康佳集团股份有限公司 版权所有</p>
+        <span>|</span><a href="/p-15.html">联系我们</a>
+        <span>|</span><a href="/p-13.html">法律声明</a>
+        <span>|</span><a href="/p-14.html">隐私保护</a>
+        <span>|</span><a href="https://m.kuaidi100.com/">快递查询</a>
+        <span>|</span><a href="http://tv.qq.com/">腾讯视频TV端</a>
+        <span>|</span><a href="http://www.beian.miit.gov.cn/" target="_blank">粤ICP备 05059413</a>
+    </div>
+</div>
+<div class="for-theme">
+    <a href="https://credit.szfw.org/CX20160830025087000485.html" target="_blank"><img src="https://www.konka.com/themes/pc/konka/images/cert3.png?vf06b" alt=""></a>
+    <a href="https://v.pinpaibao.com.cn/authenticate/cert/?site=www.konka.com&amp;at=official" target="_blank"><img src="https://www.konka.com/themes/pc/konka/images/cert2.png?vf06b" alt=""></a>
+    <a href="http://szcert.ebs.org.cn/1db11754-da73-4970-aa65-a587139e866e" target="_blank"><img src="https://www.konka.com/themes/pc/konka/images/cert.gif?vf06b" alt=""></a>
+</div>`
+$("<div></div>").html(footer1).appendTo("body")
+})()
