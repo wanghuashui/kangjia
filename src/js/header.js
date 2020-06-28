629e3f08233f1991d01a1626a6b33c6f4ddbac04
wanghuashui <wx15959172169@163.com>
2020-06-28 21:39:40 +0800

    头部转js

---
+++ src/js/header.js
@@ -0,0 +1,44 @@
+let topbar=`
+<div class="site-topbar">
+    <div class="site-topbar-container">
+        <div class="topbar-nav">
+            <a href="" title="康佳新闻" rel="nofollow" target="_blank">康佳新闻</a>
+            <span class="sep">|</span>
+            <a href="" title="加入康佳" rel="nofollow" target="_blank">加入康佳</a>
+            <span class="sep">|</span>
+            <a href="" title="校园招聘" rel="nofollow" target="_blank">校园招聘</a>
+            <span class="sep">|</span>
+            <a href="" title="康佳社区" rel="nofollow" target="_blank">康佳社区</a>
+            <span class="sep">|</span>
+            <a href="" title="大宗采购" rel="nofollow" target="_blank">大宗采购</a>
+            <span class="sep">|</span>
+            <a href="" title="积分商城" rel="nofollow" target="_blank">积分商城</a>
+            <span class="sep">|</span>
+        </div>
+        <div class="topbar-cart">
+            <a href="/cart.html" class="iconImg">
+            <img class="top-header-cart" src="https://www.konka.com/themes/pc/konka/images/top-header-cart.png?vf06b">
+                购物车(<span class="cart-num">0</span>)
+                <span class="sp-num" style="display:none"></span>
+            </a>
+        </div>
+        <div class="topbar-info">
+            <span class="spanLine"></span>
+            <a href="javascript:;" class="member-btn" id="dLabel" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="display: none"></a>
+                <a href="/passport-login.html" class=" signin-btn" style="">登录</a>
+                <span class=" sep ">|</span>
+                <a href="/passport-signup.html" class=" signup-btn" style="">注册</a>
+            <ul class="dropdown-menu" aria-labelledby="dLabel">
+                <li><a href="/my.html">个人中心</a></li>
+                <li><a href="/my-orders.html">我的订单</a></li>
+                <li><a href="/passport-logout.html">退出登录</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+`
+let header=`
+<div class="site-header" id="nav-top">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xs-2 logoImg"><a href="/index.html"><img src="https://www.konka.com/themes/pc/konka/images/logo.png?v9f33"></a></div>`
