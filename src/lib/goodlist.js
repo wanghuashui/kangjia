ad95537130780eee6605da27312e921187d79266
wanghuashui <wx15959172169@163.com>
2020-07-02 12:44:16 +0800

    添加商品，购物车

--- src/lib/goodlist.js
+++ src/lib/goodlist.js
@@ -8,14 +8,12 @@ $(() => {
         for (let i = 0; i < data.length && i <= 23; i++) {
             html += `<div class="goods-list-item" data-id="${data[i].id}">
                 <div class="figure">
-                    <a href="javascipt:void(0)">
                         <img class="layz" alt="${data[i].goodsname}" src=${data[i].src} style="display: inline;">
-                    </a>
                 </div>
                 <h4 class="text-uppercase figure-title">${data[i].goodsname}</h4>
                 <div class="figure-title figureColor">${data[i].desc}</div>
                 <p class="price">￥${data[i].price}</p>
-                <div class="wrapper" style="display: none;">
+                <div class="wrapper">
                     <a href="javascipt:void(0)" type="button" class="btn btn-default">加入购物车</a>
                     <a href="javascipt:void(0)" type="button" class="btn btn-danger">立即购买</a>
                 </div>
@@ -29,38 +27,74 @@ $(() => {
             for (let i = 0; i < item.goods.length && i <= 23; i++) {
                 html += `<div class="goods-list-item" data-id="${item.goods[i].id}">
                     <div class="figure">
-                        <a href="javascipt:void(0)">
                             <img class="layz" alt="${item.goods[i].goodsname}" src=${item.goods[i].src} style="display: inline;">
-                        </a>
                     </div>
                     <h4 class="text-uppercase figure-title">${item.goods[i].goodsname}</h4>
                     <div class="figure-title figureColor">${item.goods[i].desc}</div>
                     <p class="price">￥${item.goods[i].price}</p>
-                    <div class="wrapper" style="display: none;">
-                        <a href="javascipt:void(0)" type="button" class="btn btn-default">加入购物车</a>
-                        <a href="javascipt:void(0)" type="button" class="btn btn-danger">立即购买</a>
+                    <div class="wrapper">
+                        <div class="btn btn-default">加入购物车</div>
+                        <div type="button" class="btn btn-danger">立即购买</div>
                     </div>
                 </div>`
             }
             $(".goods-list").eq(idx + 1).html(html)
         })
     })
-    $("dt").click(function(){
-        $(".list-inline li,dt").css({background: "#fff",
-                borderBottom: "1px solid #fff"})
-        $(this).css({background: "#eee",
-                borderBottom: "1px solid #e0e0e0"})
+    $("dt").click(function () {
+        $(".list-inline li,dt").css({
+            background: "#fff",
+            borderBottom: "1px solid #fff"
+        })
+        $(this).css({
+            background: "#eee",
+            borderBottom: "1px solid #e0e0e0"
+        })
         $(".goods-list").css("display", "none")
         $(".goods-list").eq(0).css("display", "block")
     })
     $(".list-inline li").click(function () {
-        $(".list-inline li,dt").css({background: "#fff",
-                borderBottom: "1px solid #fff"})
-        $(this).css({background: "#eee",
-                borderBottom: "1px solid #e0e0e0"})
+        $(".list-inline li,dt").css({
+            background: "#fff",
+            borderBottom: "1px solid #fff"
+        })
+        $(this).css({
+            background: "#eee",
+            borderBottom: "1px solid #e0e0e0"
+        })
         let idx = $(this).index()
         $(".goods-list").css("display", "none")
         $(".goods-list").eq(idx + 1).css("display", "block")
+    })
+    // 给商品添加跳转详情页
+    $(".goods-list").on("click", ".goods-list-item", function () {
+        location.href = `./item.html?id=${$(this).data("id")}`
+    })
+    $(".goods-list").on("mouseenter", ".goods-list-item", function () {
+        $(this).find(".price").stop().fadeOut()
+        $(this).find(".wrapper").stop().fadeIn()
+    })
+    $(".goods-list").on("mouseleave", ".goods-list-item", function () {
+        $(this).find(".wrapper").stop().fadeOut()
+        $(this).find(".price").stop().fadeIn()
+    })
+    // 加入购物车表
+    $(".goods-list").on("click", ".btn-default", function () {
+        if (localStorage.getItem("userid")) {
+            $.ajax({
+                url: "../server/addcart.php",
+                data: {
+                    goodsid: $(this).parents(".goods-list-item").data("id"),
+                    userid: localStorage.getItem("userid"),
+                }
+            }).done(data=>{
+                alert(data)
+            })
+        } else {
+            alert("请先登录账号")
+            location.href = "./login.html"
+            return false
+        }
     })
     function dataTool(data) {
         let arr = []
---
+++ src/lib/item.js
@@ -0,0 +1,76 @@
+$(()=>{
+    $.ajax({
+        url:"../server/item.php",
+        data:{id:location.href.split("=")[1]},
+        dataType:"json"
+    }).done(data=>{
+        console.log(data)
+        let html=""
+        html=`<ul class="breadcrumb">
+                <li>${data[0].goodstype}></li>
+                <li class="active">${data[0].goodsname}</li>
+            </ul>
+            <div class="product-page">
+                <div class="row clearfix">
+                    <div class="product-show">
+                        <div class="product-main-image">
+                            <a href=""><img src=${data[0].src} alt=""></a>
+                            <img class="zoomImg" src=${data[0].src} alt="">
+                        </div>
+                        <div class="updown">
+                            <i></i>
+                            <ul>
+                                <li><img src=${data[0].src} alt=""></li>
+                            </ul>
+                            <i></i>
+                        </div>
+                    </div>
+                    <div class="product-buy">
+                        <h3>${data[0].goodsname}</h3>
+                        <div>${data[0].desc}</div>
+                        <div class="pricebox">
+                            <strong class="price">￥${data[0].price}</strong>
+                        </div>
+                        <div class="addressWrap">配送地址:&nbsp;<input type="text" placeholder="写了也没用"></div>
+                        <div class="product-page-cart">
+                            <div class="product-quantity input-group" data-minibuy="">
+                                <dl class="dl-horizontal">
+                                    <dt>数量</dt>
+                                </dl>
+                                <div class="spinner-buttons input-group-btn">
+                                    <button type="button" class="btn btn-default">-</button>
+                                </div>
+                                <input type="text" class="spinner-input" id="numBer" data-id="${data[0].id}" value="1">
+                                <div class="spinner-buttons input-group-btn">
+                                    <button type="button" class="btn btn-default">+</button>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <a class="btn btn-danger favorite btn-buy distanceRight" href="">立即购买</a>
+                                <a class="btn btn-warning favorite distanceRight" href="javascript:viod(0)">加入购物车</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>`
+    $(".product-container").html(html)
+    })
+
+    $(".product-container").on("click", ".btn-warning", function () {
+        if (localStorage.getItem("userid")) {
+            $.ajax({
+                url: "../server/addcart.php",
+                data: {
+                    goodsid: $("#numBer").data("id"),
+                    userid: localStorage.getItem("userid"),
+                }
+            }).done(data=>{
+                alert(data)
+            })
+        } else {
+            alert("请先登录账号")
+            location.href = "./login.html"
+            return false
+        }
+    })
+})
